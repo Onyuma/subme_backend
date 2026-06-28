b8146e7db62535c881c16ba362e81d2aeabf5b12
ONYUMA JOHN <johnonyumaudu@gmail.com>
2026-06-28 08:31:59 -0700

    Edit database config

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,19 +2,39 @@ import { Sequelize } from "sequelize-typescript";
 import configs from "./configs";
 import path from "node:path";
 
-const connection = new Sequelize({
-  dialect: "postgres",
-  host: configs.POSTGRES_HOST,
-  database: configs.POSTGRES_DB,
-  username: configs.POSTGRES_USER,
-  password: configs.POSTGRES_PASSWORD,
-  logging: false,
-  models: [path.resolve(__dirname, "../models")],
-  modelMatch: (filename, member) => {
-    return (
-      filename.substring(0, filename.indexOf(".model")) === member.toLowerCase()
-    );
-  },
-});
+let connection;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+if (!isProduction) {
+  connection = new Sequelize({
+    dialect: "postgres",
+    host: configs.POSTGRES_HOST,
+    database: configs.POSTGRES_DB,
+    username: configs.POSTGRES_USER,
+    password: configs.POSTGRES_PASSWORD,
+    logging: false,
+    models: [path.resolve(__dirname, "../models")],
+    modelMatch: (filename, member) => {
+      return (
+        filename.substring(0, filename.indexOf(".model")) ===
+        member.toLowerCase()
+      );
+    },
+  });
+} else {
+  connection = new Sequelize(configs.DATABASE_URL as string, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: isProduction
+        ? {
+            require: true,
+            rejectUnauthorized: false,
+          }
+        : false,
+    },
+  });
+}
 
 export default connection;
